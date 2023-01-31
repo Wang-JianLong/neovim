0844ba257ad847e5042cdf857fbcb4ffa7614614
wangjianlong <1939368045@qq.com>
2023-01-31 20:49:42 +0800

    2.1.1

--- lua/lsp/cpp.lua
+++ lua/lsp/cpp.lua
@@ -5,17 +5,17 @@ function M.env(sys)
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "cpp",
     callback = function()
-      local envkey = ":! g++ % -o wjl  && wjl  <CR>";
+      local envkey = ":w :! g++ % -o wjl  && wjl  <CR>";
       local tenvkey = ":terminal g++ % -o wjl && wjl<CR>i"
       if sys == 1 then
         tenvkey = ":terminal g++ % -o wjl && ./wjl<CR>i"
-        envkey = ":! g++ % -o wjl  && ./wjl <CR>"
+        envkey = ":w :! g++ % -o wjl  && ./wjl <CR>"
       end
       -- -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
       vim.keymap.set(
         "n",
         "<space>c",
-        ":! g++ % -o wjl<CR> "
+        ":w :! g++ % -o wjl<CR> "
       -- { silent = true, noremap = true }
       )
       vim.keymap.set(
